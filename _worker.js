ebaeb4ca9cf2dd870bf5ab92a27eb85d23e35726
Rico Shandika Jovial Agista <ricoagista@gmail.com>
2025-08-16 18:02:22 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,11 @@
 export default {
   async fetch(req, env) {
     const { pathname } = new URL(req.url);
-
     if (pathname === "/api/new" && req.method === "POST") {
       return new Response(JSON.stringify({ ok: true, msg: "Functions OK" }), {
         headers: { "Content-Type": "application/json" },
       });
     }
-
-    // selain /api/new tetap layani file statis dari folder public/
-    return env.ASSETS.fetch(req);
+    return env.ASSETS.fetch(req); // selain /api/new layani file di /public
   }
 }
